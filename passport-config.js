c6fdc1dac99b0782fbf85a6cbe63ecb39c52c0ce
AvneetSidhu <56938074+AvneetSidhu@users.noreply.github.com>
2021-12-29 19:07:23 -0500

    adding log-in

--- passport-config.js
+++ passport-config.js
@@ -2,30 +2,32 @@ const LocalStrategy = require('passport-local').Strategy
 const User = require('./models/usrSchema')
 const bcrypt = require('bcrypt')
 
-function initialize(passport){
-    const authenticateUser = async (email, password, done) => {
-        let user = await User.findOne({email});
-        if(user == null){
-            return done(null, false, {message: "no user with that email"})
-        }
 
-        try{
-            if (await bcrypt.compare(password, user.password)) {
-                console.log("login Success")
-                return done(null, user)
-            } else {
-                 return done(null, false, {message: "incorrect password"})
-            }
-        } catch (err) {
-            return done(err)
-        }
-    }
+module.exports = function(passport) {
+    passport.use(
+        new LocalStrategy((username, password, done) => {
+            User.findOne({usernameField : 'email'}, (err, user) => {
+                if (err) throw err;
+                if (!user) return done(null, false);
+                bcrypt.compare(password, user.password, (err, result) => {
+                    if(err) throw err;
+                    if (result == true) {
+                        return done(null, user)
+                    } else {
+                        return done(null, false);
+                    }
+                })
+            })
+        })
+    );
+    
+    passport.serializeUser((user,cb) => {
+        cb(null, user.id);
+    })
+    passport.deserializeUser((id,cb) => {
+        User.findOne({_id:id}, (err, user) => {
+            cb(err, user);
+        })
+    })
 
-    passport.use(new LocalStrategy({ usernameField: 'email' }, 
-    authenticateUser))
-
-    passport.serializeUser((user, done) => { })
-    passport.deserializeUser((user, done) => { })
-}
-
-module.exports = initialize
+};
--- server.js
+++ server.js
@@ -1,6 +1,3 @@
-if(process.env.NODE_ENV != 'production'){
-    require('dotenv').config()
-}
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
@@ -8,24 +5,34 @@ const dbURI = "mongodb+srv://user:123p@cluster0.szcic.mongodb.net/movie-app?retr
 //const dbURI = 'mongodb+srv://firstuser:123p@cluster0.drrxn.mongodb.net/to-do-app?retryWrites=true&w=majority'
 const User = require('./models/usrSchema')
 const bcrypt = require('bcrypt')
-const initializePassport = require('./passport-config')
 const passport = require('passport')
 const flash = require('express-flash')
+const cookieParser = require('cookie-parser');
 const session = require('express-session')
+
 port = 5000;
 
+// middleware 
 app.use(express.json())
-initializePassport(passport)
-app.use(flash())
 app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true
 }))
-
+app.listen(cookieParser("secret"))
 app.use(passport.initialize())
 app.use(passport.session())
+require('./passport-config')(passport);
 
+
+
+
+
+
+
+
+
+//connect to db
 
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -34,15 +41,28 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
         console.log("server started");
     })
     .catch((err) => console.log(err));
-    
+
+
+
+
+// routes
+
 app.get("/", (req, res) => {
     res.json({"users":["a","b"]})
 })
 
 app.use("/sign-up",require('./routes/signUp'))
 
-app.post('/log-in', passport.authenticate('local', {
-    successRedirect: '/home',
-    failureRedirect: '/login',
-    failureFlash: true
-}))
+app.post('/log-in', (req, res, next) => {
+    passport.authenticate('local', (err, user, info) => {
+        if (err) throw err;
+        if (!user) res.send("No user found");
+        else{
+            req.logIn(user, err => {
+                if (err) throw err;
+                res.send("successful login");
+                console.log(req.user)
+            });
+        }
+    })(req,res,next);
+})
